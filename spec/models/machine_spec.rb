8c6adcd40088717b6ed8b703b42a07850dae2735
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-24 09:27:52 -0600

    Add has_many machine_snacks relationships to Snacks and Machines

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Machine, type: :model do
+  describe 'relationships' do
+    it {should have_many :snacks}
+    it {should belong_to :owner}
+  end
+
   describe 'validations' do
     it { should validate_presence_of :location }
     it { should belong_to :owner }
--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Snack, type: :model do
+  describe 'relationships' do
+    it {should have_many :machines}
+  end
+
   describe 'validations' do
     it {should validate_presence_of :name}
     it {should validate_presence_of :price}
