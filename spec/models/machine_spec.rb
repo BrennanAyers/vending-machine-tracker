f691a60443db4f515eb5a83d1ec593467d34cf94
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-05-24 09:37:22 -0600

    Add Machine Model spec for #average_price

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -10,4 +10,18 @@ RSpec.describe Machine, type: :model do
     it { should validate_presence_of :location }
     it { should belong_to :owner }
   end
+
+  describe 'instance methods' do
+    before :each do
+      @owner = Owner.create(name: "Sam's Snacks")
+      @dons  = @owner.machines.create(location: "Don's Mixed Drinks")
+      @white_castle = @dons.snacks.create!(name: "White Castle Burger", price: 3.0)
+      @pop_rocks = @dons.snacks.create!(name: "Pop Rocks", price: 1.0)
+      @cheetos = @dons.snacks.create!(name: "Flaming Hot Cheetos", price: 2.0)
+    end
+
+    it '#average_price' do
+      expect(@dons.average_price).to eq(2.0)
+    end
+  end
 end
